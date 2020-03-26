0186420d251cc8502e4e79a5a3efe7d0efc2803e
emotiontech <emotiontech@mbpdeemtiontech.home>
2020-03-26 11:21:59 +0100

    deploy

---
+++ taskbox/storybook-static/precache-manifest.e63e08bdd3a5ab7db313733cbe297cdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249172825eb67c6235662e09d35024cc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.60d823493cac0eb6671d.bundle.js"
+  },
+  {
+    "url": "runtime~main.60d823493cac0eb6671d.bundle.js"
+  },
+  {
+    "revision": "221faf34bc8015038bea",
+    "url": "static/css/main.eda11504.chunk.css"
+  },
+  {
+    "url": "vendors~main.60d823493cac0eb6671d.bundle.js"
+  },
+  {
+    "url": "vendors~main.60d823493cac0eb6671d.bundle.js.LICENSE.txt"
+  }
+]);
